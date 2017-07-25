bdc34c5ccfa129ee8cb311d245f8d61c75f84a56
zawzaw-playerz <mczawzaw12@outlook.com>
2017-07-25 22:01:13 +0630

    First Commit

---
+++ app/src/main/java/substratum/zawzaw/purezmaterialblue/internal/TBOConstants.kt
@@ -0,0 +1,15 @@
+package substratum.zawzaw.purezmaterialblue.internal
+
+object TBOConstants {
+
+    val THEME_READY_PACKAGES = arrayOf(
+            "com.google.android.gm",
+            "com.google.android.googlequicksearchbox",
+            "com.android.vending",
+            "com.google.android.apps.plus",
+            "com.google.android.talk",
+            "com.google.android.youtube",
+            "com.google.android.inputmethod.latin"
+    )
+
+}
